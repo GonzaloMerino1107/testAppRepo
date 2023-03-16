b32108faaab27cfc05f72db6ee4e6737c4f53043
gonmerino95@gmail.com <gonmerino95@gmail.com>
2023-03-16 10:32:11 +0100

    Test commit

---
+++ test.js
@@ -0,0 +1,9 @@
+console.log("Hola mundo");
+
+var prueba = 5;
+
+if(prueba === 4){
+  console.log('El valor de la variable prueba es igual a 4')
+}else{
+  console.log(`El valor de la variable prueba es distinto de 4`)
+}
